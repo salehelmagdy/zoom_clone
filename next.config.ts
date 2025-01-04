0a941f5c6b5ec6422dfbc594998ed232e5313294
unknown <salehelmagdy5@gmail.com>
2025-01-04 16:57:46 +0200

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    appDir: true,
+    turbo: true, // Ensure Turbopack is enabled
+  },
 };
 
 export default nextConfig;
